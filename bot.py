92dddd2dab34c169a7637c996abd3482700d9294
unoterr <temnyy1990@gmail.com>
2021-02-14 16:38:38 +0100

    SoSo

--- bot.py
+++ bot.py
@@ -1,6 +1,13 @@
 from aiogram import executor
-from conf import dp
+from conf import dp, bot
 import handlers
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
+
+chatId = "-1001161219382"
+
+async def inputRead():
+    input_r = input() 
+    print(input_r)
+    await bot.send_message(chatId, input_r)
--- handlers/general.py
+++ handlers/general.py
@@ -1,8 +1,6 @@
 from aiogram import types
 from conf import dp, bot
 
-chatId = "-1001161219382"
-
 @dp.message_handler(commands=["start"])
 async def cmd_start(message: types.Message):
     await message.reply("Привет! Я - Капабара!")
@@ -10,8 +8,3 @@ async def cmd_start(message: types.Message):
 @dp.message_handler()
 async def echo(message: types.Message):
     await message.answer(message.text)
-
-async def inputRead():
-    input_r = input() 
-    print(input_r)
-    await bot.send_message(chatId, input_r)
