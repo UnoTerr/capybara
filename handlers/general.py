89d785eb806ed20d7c266c79119391567a07b564
unoterr <temnyy1990@gmail.com>
2021-02-14 16:36:32 +0100

    Read Input

--- handlers/general.py
+++ handlers/general.py
@@ -1,6 +1,8 @@
 from aiogram import types
 from conf import dp, bot
 
+chatId = "-1001161219382"
+
 @dp.message_handler(commands=["start"])
 async def cmd_start(message: types.Message):
     await message.reply("Привет! Я - Капабара!")
@@ -8,3 +10,7 @@ async def cmd_start(message: types.Message):
 @dp.message_handler()
 async def echo(message: types.Message):
     await message.answer(message.text)
+
+async def inputRead():
+    input_r = input() 
+    await bot.send_message(chatId, input_r)
