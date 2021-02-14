65a345a2563c7b316d995c776182f005a2c61925
unoterr <temnyy1990@gmail.com>
2021-02-14 18:28:42 +0100

    New book

--- handlers/new.py
+++ handlers/new.py
@@ -1,5 +1,6 @@
 from aiogram import types, Bot
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from conf import dp, bot
 
@@ -10,7 +11,7 @@ class New(StatesGroup):
     book_link = State()
     book_date = State()
 
-@dp.message_handler(lambda message: message.text == "Новая книга", state="*")
+@dp.message_handler(Text(equals="новая книга", ignore_case=True), state="*", is_chat_admin=True)
 async def new_book_1(message: types.Message):
     conn = await aiosqlite.connect('mybd.db')
     c = await conn.cursor()
@@ -20,19 +21,19 @@ async def new_book_1(message: types.Message):
     await message.answer("Пиши название: ")
     await New.book_name.set()
 
-@dp.message_handler(state = New.book_name, content_types=types.ContentTypes.TEXT)
+@dp.message_handler(state = New.book_name, content_types=types.ContentTypes.TEXT, is_chat_admin=True)
 async def new_book_2(message: types.Message, state: FSMContext):
     await state.update_data(name_n = message.text.lower())
     await message.answer("Давай ссылку: ")
     await New.book_link.set()
 
-@dp.message_handler(state = New.book_link, content_types=types.ContentTypes.TEXT)
+@dp.message_handler(state = New.book_link, content_types=types.ContentTypes.TEXT, is_chat_admin=True)
 async def new_book_3(message: types.Message, state: FSMContext):
     await state.update_data(link_n = message.text.lower())
     await New.next()
     await message.answer("Говори дату в формате ДД.ММ.ГГГГ: ")
 
-@dp.message_handler(state = New.book_date, content_types=types.ContentTypes.TEXT)
+@dp.message_handler(state = New.book_date, content_types=types.ContentTypes.TEXT, is_chat_admin=True)
 async def new_book_4(message: types.Message, state: FSMContext):
     book_data = await state.get_data()
     date_n = message.text.lower()
