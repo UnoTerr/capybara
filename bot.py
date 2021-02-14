91901cff18919ee5a6254e457750b3a340eaf8b8
unoterr <temnyy1990@gmail.com>
2021-02-14 16:44:59 +0100

    So?

--- bot.py
+++ bot.py
@@ -4,11 +4,10 @@ import handlers
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
-    inputRead()
 
 chatId = "-1001161219382"
 
-async def inputRead():
-    input_r = input() 
-    print(input_r)
-    await bot.send_message(chatId, input_r)
+async def send(text):
+#    input_r = input() 
+#    print(input_r)
+    await bot.send_message(chatId, text)
