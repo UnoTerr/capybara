b2e695fd210e82c7723975c28877dca58f352efe
unoterr <temnyy1990@gmail.com>
2021-02-17 15:37:17 +0100

    Alert

--- handlers/proc.py
+++ handlers/proc.py
@@ -56,7 +56,7 @@ async def alert():
     d0 = today.strftime("%d.%m.%Y")
     d1 = datetime.strptime(d0, '%d.%m.%Y')
     conn = await aiosqlite.connect('mybd.db')
-    async with conn.execute("SELECT id, date, name FROM books WHERE status = 'disp'") as cursor:
+    async with conn.execute("SELECT id, date, name FROM books WHERE status = 'disp' ORDER BY id DESC LIMIT 1") as cursor:
         async for i in cursor:
             d2 = datetime.strptime(i[1], '%d.%m.%Y')
             dif = abs((d2 - d1).days)
