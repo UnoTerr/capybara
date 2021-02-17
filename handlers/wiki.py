cf14ccac4f07e405665786e37d2d7e263acf148e
unoterr <temnyy1990@gmail.com>
2021-02-17 14:43:56 +0100

    Debug

--- handlers/wiki.py
+++ handlers/wiki.py
@@ -7,5 +7,8 @@ from aiogram.dispatcher.filters import Text
 @dp.message_handler(Text(equals='Найти на вики', ignore_case=True))
 async def wiki(message: types.Message):
     txt = message.text.split(' ', 3)[3]
+    print(txt)
     wikipedia.set_lang("ru")
-    await message.reply(wikipedia.summary(txt, sentences=3))
+    msg = wikipedia.summary(txt, sentences=3)
+    print(msg)
+    await message.reply(msg)
