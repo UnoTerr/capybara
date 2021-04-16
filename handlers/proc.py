05377a772653076287de252d3d3ae2c0d9720844
unoterr <temnyy1990@gmail.com>
2021-04-16 09:09:34 +0200

    Debug

--- handlers/proc.py
+++ handlers/proc.py
@@ -64,6 +64,7 @@ async def alert():
     async with conn.execute("SELECT id, date, name FROM books WHERE status = 'disp' ORDER BY id DESC LIMIT 1") as cursor:
         async for i in cursor:
             d2 = datetime.strptime(str(i[1]), '%d.%m.%Y')
+            print(d2)
             dif = abs((d2 - d1).days)
             msg = "До окончания прочтения книги <b>{}</b> осталось {} дней".format(i[2], dif)
             if dif == 10 or dif == 5 or dif == 1:
